f61252c5236d69f15d42686a276f1c96b72eb450
sandraprog <sandraprog@mail.ru>
2019-10-16 20:53:07 +0300

    Hometask_3 #7

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -15,15 +15,16 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
 
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
-        return  if (!question.validateInput(answer)){
+        return if (answer.isNullOrEmpty()) {
             "${question.validationString}\n${question.question}" to status.color
-        }
-        else if (question.answers.contains(answer.toLowerCase())) {
+        } else if (!question.validateInput(answer)) {
+            "${question.validationString}\n${question.question}" to status.color
+        } else if (question.answers.contains(answer.toLowerCase())) {
             question = question.nextQuestion()
-			if (question != Question.IDLE)
-				"Отлично - ты справился\n${question.question}" to status.color
-			else 
-				"Отлично - ты справился\nНа этом все, вопросов больше нет" to status.color
+            if (question != Question.IDLE)
+                "Отлично - ты справился\n${question.question}" to status.color
+            else
+                "Отлично - ты справился\nНа этом все, вопросов больше нет" to status.color
         } else {
             status = status.nextStatus()
             if (status != Status.NORMAL)
@@ -47,46 +48,70 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         }
     }
 
-    enum class Question(val question: String, val answers: List<String>, val validationString: String) {
-        NAME("Как меня зовут?", listOf("Бендер", "bender"), "Имя должно начинаться с заглавной буквы") {
+    enum class Question(
+        val question: String,
+        val answers: List<String>,
+        val validationString: String
+    ) {
+        NAME(
+            "Как меня зовут?",
+            listOf("Бендер", "bender"),
+            "Имя должно начинаться с заглавной буквы"
+        ) {
             override fun nextQuestion(): Question = PROFESSION
             override fun validateInput(answer: String): Boolean {
                 return answer[0].isUpperCase()
             }
         },
-        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender"), "Профессия должна начинаться со строчной буквы") {
+        PROFESSION(
+            "Назови мою профессию?",
+            listOf("сгибальщик", "bender"),
+            "Профессия должна начинаться со строчной буквы"
+        ) {
             override fun nextQuestion(): Question = MATERIAL
-            override fun validateInput(answer:String): Boolean {
+            override fun validateInput(answer: String): Boolean {
                 return answer[0].isLowerCase()
             }
         },
-        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood"), "Материал не должен содержать цифр") {
+        MATERIAL(
+            "Из чего я сделан?",
+            listOf("металл", "дерево", "metal", "iron", "wood"),
+            "Материал не должен содержать цифр"
+        ) {
             override fun nextQuestion(): Question = BDAY
-            override fun validateInput(answer:String): Boolean {
+            override fun validateInput(answer: String): Boolean {
                 return answer.none { it -> it.isDigit() }
             }
         },
-        BDAY("Когда меня создали?", listOf("2993"), "Год моего рождения должен содержать только цифры") {
+        BDAY(
+            "Когда меня создали?",
+            listOf("2993"),
+            "Год моего рождения должен содержать только цифры"
+        ) {
             override fun nextQuestion(): Question = SERIAL
-            override fun validateInput(answer:String): Boolean {
+            override fun validateInput(answer: String): Boolean {
                 return answer.isDigitsOnly()
             }
         },
-        SERIAL("Мой серийный номер?", listOf("2716057"), "Серийный номер содержит только цифры, и их 7") {
+        SERIAL(
+            "Мой серийный номер?",
+            listOf("2716057"),
+            "Серийный номер содержит только цифры, и их 7"
+        ) {
             override fun nextQuestion(): Question = IDLE
-            override fun validateInput(answer:String): Boolean {
+            override fun validateInput(answer: String): Boolean {
                 return (answer.isDigitsOnly() && answer.length == 7)
             }
         },
         IDLE("На этом все, вопросов больше нет", listOf(), "") {
             override fun nextQuestion(): Question = IDLE
-            override fun validateInput(answer:String): Boolean {
+            override fun validateInput(answer: String): Boolean {
                 return true
             }
         };
 
         abstract fun nextQuestion(): Question
-        abstract fun validateInput(answer:String): Boolean
+        abstract fun validateInput(answer: String): Boolean
     }
 
 }
